b20d4dead8e72fbf5d35895206b3c383e76420f6
AnaLucia Rojas <analuciarojas@AnaLucias-MacBook-Pro.local>
2021-12-14 16:51:42 -0800

    started question prompts and commented write readmefile function

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,53 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ()=> {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project? (Required)',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project title!');
+                    return false;
+                }
+            }
+          },
+          
+
+    ])
+
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+          return promptProject(portfolioData);
+        } else {
+          return portfolioData;
+        }
+      });
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+/*function writeToFile(fileName, data) {
+    fs.writeFile(`./dist/${fileName}`, data, err => {
+        if (err) {
+            throw err
+        };
+        console.log('README file created!')
+    });
+};*/
 
-// Function call to initialize app
-init();
+//TODO: Create a function to initialize app
+
+questions()
+  .catch(err => {
+    console.log(err);
+  });
+
+
